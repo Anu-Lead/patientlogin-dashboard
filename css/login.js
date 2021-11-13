d0aec4945e693b4cf7af8e67e4f6d926ba46b274
Anuoluwa <anuoluwaakinsola1@gmail.com>
2021-11-13 13:08:43 +0100

    Add Css

---
+++ css/login.js
@@ -0,0 +1,39 @@
+let email = document.forms['form']['email'];
+let password = document.forms['form']['password'];
+
+
+let email_error = document.getElementById('email_error');
+let pwd_error = document.getElementById('passw_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+
+function validation(){
+    if (email.value.length < 9){
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if (password.value.length < 6){
+        password.style.border = "1px solid red";
+        passw_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+}
+
+function email_Verify(){
+    if (email.value.length >= 8)
+        email.style.border = "1px solid sliver";
+        email_error.style.display = "none";
+        return true;
+}
+
+function pass_Verify(){
+    if (password.value.length >= 8)
+        password.style.border = "1px solid sliver";
+        passw_error.style.display = "none";
+        return true;
+}
